fd259073bfa2bbe1d2688fd636a954823c2166ae
shohag390 <shohag390@users.noreply.github.com>
2025-02-08 23:01:47 +0600

    for loop add

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-let num = 30;
+// let num = 30;
 
-while (num <= 50) {
-    console.log(num);
-    num++
+// while (num <= 50) {
+//     console.log(num);
+//     num++
 
-}
+// }
 
 
-// 1 thake 10 all sum 
-let number = 1;
+// // 1 thake 10 all sum 
+// let number = 1;
+// let sum = 0;
+
+// while (number <= 10) {
+//     console.log(number);
+//     sum = sum + number
+//     console.log("sum :", sum);
+//     number++
+
+// }
+
+
+// let num = 1;
+
+// while (num <= 20) {
+//     console.log(num);
+
+//     if (num % 2 === 0) {
+//         console.log("Even No :", num);
+//     }
+//     num++
+// }
+// let num2 = 1;
+
+// while (num2 <= 20) {
+//     console.log(num2);
+
+//     if (num2 % 2 === 0) {
+//         console.log("Odd No :", num2);
+//     }
+//     num++
+// }
+
 let sum = 0;
 
-while (number <= 10) {
-    console.log(number);
-    sum = sum + number
-    console.log("sum :", sum);
-    number++
-
+for (let i = 1; i <= 30; i++) {
+    sum = sum + i;
+    console.log(i);
 }
+
+console.log("All Sum :", sum);
